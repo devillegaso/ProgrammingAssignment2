507501e5de5e493620c74c0ad816023d632339e0
Daniel Villegas <danielvillegasortiz@gmail.com>
2020-08-09 16:34:42 -0500

    My version of the assignment is uploaded

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These functions create a matrix an calculate its
+## inverse. Note: the matrix must be small in order
+## for the functions to work.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix which is 
+## actually a list of characteristics
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<-NULL
+  }
+  get<-function()x
+  setinv<-function(inverse)inv<<-inverse
+  getinv<-function()inv
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the matrix and calculates its
+## inverse. The introduced matrix should be square.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getmean()
+  if(!is.null(inv)){
+    message("geting cached data")
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data,...)
+  x$setinv(inv)
+  inv
 }
